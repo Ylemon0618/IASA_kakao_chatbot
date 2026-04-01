c934375020f9819eea4ea8977241a91c4a011917
ylemon <kimkyuwan0618@gmail.com>
2026-04-01 17:08:40 +0900

    feat: add searching today schedule

--- routes/schedule_search.js
+++ routes/schedule_search.js
@@ -16,32 +16,32 @@ function getRotatedTeacher(teachers, startDate, offset = 0) {
     return teachers[teacherIndex];
 }
 
-router.post('/tomorrow', async (req, res) => {
-    const userId = req.body.userRequest.user.id;
-
-    const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-    const daysKo = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
-    const tomorrowIndex = (new Date().getDay() + 1) % 7;
-    const tomorrowDay = days[tomorrowIndex];
-    const tomorrowDayKo = daysKo[tomorrowIndex];
-
+async function getSchedules(req, res, isTomorrow = False) {
     try {
-        const data = await Timetable.findOne({userId: userId, day: tomorrowDay});
+        const userId = req.body.userRequest.user.id;
+
+        const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+        const daysKo = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+        const scheduleDayIndex = (isTomorrow ? new Date().getDay() + 1 : new Date().getDay()) % 7;
+        const scheduleDay = days[scheduleDayIndex];
+        const scheduleDayKo = daysKo[scheduleDayIndex];
+
+        const data = await Timetable.findOne({userId: userId, day: scheduleDay});
 
         if (!data || !data.schedule || data.schedule.length === 0) {
             return res.json({
                 version: "2.0",
-                template: {outputs: [{simpleText: {text: `📅 내일(${tomorrowDay})은 등록된 시간표가 없습니다.`}}]}
+                template: {outputs: [{simpleText: {text: `📅 ${isTomorrow ? "내일" : "오늘"}(${scheduleDay})은 등록된 시간표가 없습니다.`}}]}
             });
         }
 
-        const carouselItems = data.schedule.slice(0, 9).map((item) => {
+        return data.schedule.slice(0, 9).map((item) => {
             const teacher = getRotatedTeacher(item.teacher, item.rotationDate);
 
             let items = {
                 imageTitle: {
                     title: `${item.period}교시: ${item.subject}`,
-                    description: `IASA Tomorrow Schedule`,
+                    description: `IASA Schedule`,
                 },
                 itemList: [
                     {
@@ -58,39 +58,47 @@ router.post('/tomorrow', async (req, res) => {
                     {
                         action: "message",
                         label: `이번주 담당 교사 지정하기`,
-                        messageText: `${tomorrowDayKo} ${item.period}교시 담당 교사 지정해줘`
+                        messageText: `${scheduleDayKo} ${item.period}교시 담당 교사 지정해줘`
                     }
                 ]
             }
 
             return items
         });
+    } catch (error) {
+        console.error('error while searching schedule:', err);
+        return null;
+    }
+}
 
-        return res.json({
-            version: "2.0",
-            template: {
-                outputs: [
-                    {
-                        carousel: {
-                            type: "itemCard",
-                            items: carouselItems
-                        }
-                    }
-                ],
-                quickReplies: [
-                    {label: "오늘 시간표 보기", action: "message", messageText: "오늘 시간표 알려줘"},
-                    {label: "급식 확인하기", action: "message", messageText: "내일 급식 뭐야"}
-                ]
-            }
-        });
+router.post('/day', async (req, res) => {
+    const isTomorrow = req.body.action.params.isTomorrow === "true";
+    const carouselItems = getSchedules(req, res, isTomorrow);
 
-    } catch (err) {
-        console.error('조회 에러:', err);
+    if (!carouselItems) {
         return res.json({
             version: "2.0",
             template: {outputs: [{simpleText: {text: "시간표를 불러오는 중 서버 오류가 발생했습니다."}}]}
         });
     }
+
+    return {
+        version: "2.0",
+        template: {
+            outputs: [
+                {
+                    carousel: {
+                        type: "itemCard",
+                        items: carouselItems
+                    }
+                }
+            ],
+            quickReplies: [
+                {label: "오늘 시간표 보기", action: "message", messageText: "오늘 시간표 알려줘"},
+                {label: "급식 확인하기", action: "message", messageText: "내일 급식 뭐야"}
+            ]
+        }
+    };
 });
 
 module.exports = router;
