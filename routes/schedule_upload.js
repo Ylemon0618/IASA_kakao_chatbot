6a370e396cd9bbe36f8262b1f220a288a33bf024
ylemon <kimkyuwan0618@gmail.com>
2026-04-01 09:52:16 +0900

    test: delete test logging

--- routes/schedule_upload.js
+++ routes/schedule_upload.js
@@ -105,7 +105,6 @@ router.post('/register/name', async (req, res) => {
 });
 
 router.post('/register/teacher', async (req, res) => {
-    console.log(req.body.action.params)
     const userId = req.body.userRequest.user.id;
     const mondaySchedule = req.body.action.params.monday;
     const tuesdaySchedule = req.body.action.params.tuesday;
