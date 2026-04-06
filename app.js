b5076c096d4adf9c8e90277d70ca82b7906b30e3
ylemon <kimkyuwan0618@gmail.com>
2026-04-06 09:07:36 +0900

    test: test commit

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use('/api/iasa/schedule/search', scheduleSearchRouter);
 app.use('/api/iasa/schedule/set', scheduleSetRouter);
 
 const PORT = Number(process.env.PORT) || 25565;
+
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`server on ${PORT}`);
 });
