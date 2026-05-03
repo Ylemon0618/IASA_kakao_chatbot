55bbb7059eae363eb23f6b7200ed839b005adba8
Kyuwan Kim <kimkyuwan0618@gmail.com>
2026-05-03 22:17:19 +0900

    chore: update goorm answer

--- goorm_add.js
+++ goorm_add.js
@@ -14,133 +14,151 @@ return Goorm.insertOne(
         problems: [
             {
                 number: 1,
-                code: `import sys
-inputs = sys.stdin.readlines()
-
-arr = list(map(int, inputs[0].split()))
-arr.append(int(inputs[1]))
-arr.extend(list(map(int, inputs[3].split())))
-arr.insert(*list(map(int, inputs[4].split())))
-arr = [i for i in arr if i != int(inputs[5])]
-arr.sort()
-
-print(f"{int(inputs[6])} 재고 수량: {arr.count(int(inputs[6]))}")
-print(f"{int(inputs[7])}의 인덱스: {arr.index(int(inputs[7]))}")
-print(f"최종 재고: {arr[::-1]}")
-arr.clear()
-print(f"재고 초기화: {arr}")`
+                code: `n, sqrt = int(input()), 0
+while sqrt ** 2 <= n:
+	sqrt += 0.001
+print(f"{sqrt - 0.001:.3f}")`
             },
             {
                 number: 2,
-                code: `x, w, z = map(int, input().split())
-print(f"{(8 * (2 * x ** 3 + 5 * x + 2) / (7 * w - 3 / z) - z) / (4 * (3 + x) / 7):.2f}")`
+                code: `n, cnt = int(input()), 0
+for x in range(-n, n + 1):
+	for y in range(-n, n + 1):
+		if x ** 2 + y ** 2 <= n ** 2:
+			cnt += 1
+print(cnt)
+print(f"{cnt / n ** 2:.4f}")`
             },
             {
                 number: 3,
-                code: `n, res = int(input()), 0
-for i in range(1, 10):
-    res += i * n
-print(res)`
+                code: `fibo = [0, 1]
+for i in range(int(input()) - 1):
+	fibo.append(fibo[-1] + fibo[-2])
+print(*fibo[1:])`
             },
             {
                 number: 4,
-                code: `n, idx = int(input()), 0
-while idx < n:
-    idx += 1
-    if any(i in str(idx) for i in ['3', '6', '9']):
-        print('x', end=' ')
-    else:
-        print(idx, end=' ')`
+                code: `fibo = [1, 1]
+for _ in range(int(input()) - 2):
+	fibo.append(fibo[-1] + fibo[-2])
+for i in range(len(fibo) - 1):
+	print(f"{fibo[i + 1] / fibo[i]:.6f}")`
             },
             {
                 number: 5,
-                code: `print(sum([i for i in range(int(input())) if not i % 3 or not i % 5]))`
+                code: `t, r, target = map(int, input().split())
+level = 0
+while t > target:
+	level += 1
+	t = t * (1 - r / 100)
+	print(f"{t:.2f}", end=' ')
+print(f"\n{level}")`
             },
             {
                 number: 6,
-                code: `n, k = map(int, input().split())
-for i in range(n, k + 1):
-    if not i % 5: print('Fizz', end='')
-    if not i % 7: print('Buzz', end='')
-    if i % 5 and i % 7: print(i, end='')
-    print()`
+                code: `dic = {idx:len(idx) for idx in input().split()}
+for key, value in sorted(dic.items()):
+	print(key, value)`
             },
             {
                 number: 7,
-                code: `n, turn = input().split()
-board = [0] * 10
-for _ in range(int(n)):
-    pos, player = input().split()
-    board[int(pos)] = 1 if player == 'O' else -1
-
-win = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-res = []
-for line in win:
-    if board[line[0]] + board[line[1]] + board[line[2]] == (2 if turn == 'O' else -2):
-        res.append(*[i for i in line if not board[i]])
-
-if not res:
-    print('Keep going')
+                code: `arr = [input().split() for _ in range(int(input()))]
+dic = {i[0]:int(i[1]) for i in arr if int(i[1]) >= 80}
+if dic:
+	for key, value in sorted(dic.items()):
+		print(key, value)
 else:
-    print(*sorted(res))`
+	print("없음")`
             },
             {
                 number: 8,
-                code: `n, board = int(input()), [0] * 10
-
-if not n:
-    print(1, 2, 3, 4, 5, 6, 7, 8, 9)
-else:
-    for i in map(int, input().split()):
-        board[int(i)] = 1
-    
-    win = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-    res = []
-    for i in range(1, 10):
-        if board[i]:
-            continue
-    
-        escape = False
-        for line in win:
-            if i in line and board[line[0]] + board[line[1]] + board[line[2]] >= 2:
-                escape = True
-                break
-    
-        if not escape:
-            res.append(i)
-    
-    if res:
-        print(*res)
-    else:
-        print('GiveUp')`
+                code: `dic = dict(zip(input().split(), input().split()))
+for key, value in dic.items():
+	print(key, value)`
             },
             {
                 number: 9,
-                code: `scores = dict(zip(['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F'], [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0]))
-credit_sum, score_sum = 0, 0
-for _ in range(int(input())):
-    name, credit, score = input().split()
-    if score != 'P':
-        credit_sum += int(credit)
-        score_sum += scores[score] * int(credit)
-print(f"{score_sum / credit_sum:.2f}")
-print("졸업 가능" if score_sum / credit_sum >= 3.1 else "졸업 불가")`
+                code: `n = int(input())
+dic = {key:value for value, key in [input().split() for _ in range(n)]}
+for key, value in sorted(dic.items()):
+	print(key, value)`
             },
             {
                 number: 10,
-                code: `a, c = map(int, input().split())
-res_cnt = 0
-for b in range(1, 101):
-    d2 = (a * b ** 2 - 100) / c
-    if d2 > 0:
-        d = d2 ** 0.5
-        if int(d) == d:
-            res_cnt += 1
-            print(b, int(d))
+                code: `arr = input().replace(' ', '')
+dic = {char:arr.count(char) for char in arr}
+for key, value in sorted(dic.items()):
+	print(key, value)`
+            },
+            {
+                number: 11,
+                code: `print(*sorted(set(map(int, input().split()))))`
+            },
+            {
+                number: 12,
+                code: `s1, s2 = set(input().split()), set(input().split())
+print(len(s1 & s2))
+print(*sorted(s1 & s2))`
+            },
+            {
+                number: 13,
+                code: `students, attendant = set(input().split()), set(input().split())
+not_attend = students - attendant
+print(len(not_attend))
+print(' '.join(sorted(not_attend)) if not_attend else "전원 참가")`
+            },
+            {
+                number: 14,
+                code: `a, b = set(input().split()), set(input().split())
+print("YES" if a <= b else "NO")`
+            },
+            {
+                number: 15,
+                code: `a, b = set(input().split()), set(input().split())
+print("YES" if a < b else "NO")`
+            },
+            {
+                number: 16,
+                code: `s1, s2, s3 = set(input().split()), set(input().split()), set(input().split())
+s4 = (s1 | s2) - s3
+print(len(s4))
+print(*sorted(s4) if s4 else '')`
+            },
+            {
+                number: 17,
+                code: `s1, s2, s3 = set(input().split()), set(input().split()), set(input().split())
+s4 = (s1 ^ s2) & s3
+print(len(s4))
+print(*sorted(s4) if s4 else '')`
+            },
+            {
+                number: 18,
+                code: `s1, s2, s3 = set(input().split()), set(input().split()), set(input().split())
+s4 = ((s1 & s2) | (s2 & s3) | (s3 & s1)) - (s1 & s2 & s3)
+print(len(s4))
+print(*sorted(s4) if s4 else '')`
+            },
+            {
+                number: 19,
+                code: `s = [set(input().split()) for _ in range(5)]
 
-if not res_cnt:
-    print('없음')`
-            }
+print(len(s[0] - (s[1] | s[2] | s[3] | s[4])))
+
+one, two = [], []
+for idx in s[0] | s[1] | s[2] | s[3] | s[4]:
+	cnt = 0
+	for i in range(5):
+		if idx in s[i]:
+			cnt += 1
+
+	if cnt == 1:
+		one.append(idx)
+	if cnt >= 2:
+		two.append(idx)
+
+print(*sorted(two))
+print(len(one))`
+            },
         ]
     },
     {
