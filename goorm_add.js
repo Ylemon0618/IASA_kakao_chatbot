63f7589a03fc86f6fee76c8843de97968d6d4bd8
Kyuwan Kim <kimkyuwan0618@gmail.com>
2026-05-22 15:19:19 +0900

    chore: update goorm_add.js

--- goorm_add.js
+++ goorm_add.js
@@ -22,227 +22,126 @@ return Goorm.insertOne(
         problems: [
             {
                 number: 1,
-                code: `print(open('data/test.txt').read())`
+                code: `def solution(arr, value):
+    return arr.index(value) if value in arr else -1`
             },
             {
                 number: 2,
-                code: `with open('test.txt', 'w') as f:
-f.write(input())
-with open('test.txt', 'r') as f:
-    print(f.read())`
+                code: `def solution(arr):
+    return sum(1 for _ in arr)`
             },
             {
                 number: 3,
-                code: `n = int(input())
-with open('numbers.txt', 'w') as f:
-\tfor i in range(n):
-\t\tf.write(input() + '\\n')
-with open('numbers.txt', 'r') as f:
-\tnum = list(map(int, f.read().split('\\n')[:-1]))
-print(max(num))`
+                code: `def solution(arr):
+    m = 10e9
+    res = 0
+    for i in range(len(arr)):
+        if arr[i] < m:
+            m, res = arr[i], i
+    return res`
             },
             {
                 number: 4,
-                code: `n = int(input())
-with open('scores.txt', 'w') as f:
-\tfor _ in range(n):
-\t\tf.write(input() + '\\n')
-with open('scores.txt', 'r') as f:
-\tarr = list(map(int, f.read().split('\\n')[:-1]))
-print(f"합계: {sum(arr)}")
-print(f"평균: {sum(arr) / n:.2f}")`
+                code: `def solution(s, c):
+    solv = ''
+    for i in s:
+        if i != c:
+            solv += i
+    return solv`
             },
             {
                 number: 5,
-                code: `n = int(input())
-num = list(map(int, open("data/randomInt.txt").read().split()))
-cnt = 0
-for i in num:
-\tif i > n:
-\t\tcnt += 1
-print(cnt)`
+                code: `def solution(s, sub):
+    solv, idx = 0, 0
+    while idx <= len(s) - len(sub):
+        if s[idx:idx + len(sub)] == sub:
+            solv += 1
+            idx += len(sub)
+        else:
+            idx += 1
+    return solv`
             },
             {
                 number: 6,
-                code: `students = []
-for line in open('data/noseTouch.txt').readlines():
-\ta, b = line.split()
-\tstudents.append(int(b))
-n = int(input())
-res = 0
-for cnt in students:
-\tif cnt >= n:
-\t\tres += 1
-print(res)`
+                code: `n = int(input())
+arr = [input().split() for _ in range(n)]
+dev = {idx[0]: max(map(int, idx[1:])) - min(map(int, idx[1:])) for idx in arr}
+for key, value in sorted(dev.items(), key=lambda x: (-x[1], x[0])):
+    print(f"{key}: {value}")`
             },
             {
                 number: 7,
-                code: `i, j = map(int, input().split())
-                with open('data/numA.txt', 'r') as f:
-lines = f.read().split('\\n')
-a = int(lines[i - 1])
-with open('data/numB.txt', 'r') as f:
-    lines = f.read().split('\\n')
-b = int(lines[j - 1])
-print(a + b)`
+                code: `count = {}
+for i in input().split():
+    count[i] = count.get(i, 0) + 1
+for key, value in sorted(count.items(), key=lambda x: (-x[1], x[0]))[:int(input())]:
+    print(f"{key}: {value}")`
             },
             {
                 number: 8,
-                code: `n, m = map(int, input().split())
-with open('data/numbers.txt', 'r') as f:
-\tlines = list(map(int, f.read().split('\\n')[:-1]))
-print(sum(lines[n - 1:m]))`
+                code: `for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    d = b ** 2 - 4 * a * c
+    if d > 0:
+        print("두 실근")
+    elif d == 0:
+        print("중근")
+    else:
+        print("허근")`
             },
             {
                 number: 9,
-                code: `s, n = input(), int(input())
-print(*[s[i:i + n] for i in range(len(s) - n + 1)], sep='\\n')`
+                code: `n = int(input())
+log = [input().split(':') for _ in range(n)]
+
+l, q = 0, 0
+cnt = {}
+for name, content in log:
+    cnt[name] = cnt.get(name, 0) + 1
+arr = sorted(cnt.items(), key = lambda x: -x[1])
+
+print(f"최다발언: {arr[0][0]}({arr[0][1]}개)")
+l = len(list(filter(lambda x: x[1].count("ㅋ") >= 3, log)))
+q = len(list(filter(lambda x: '?' in x[1], log)))
+print(f"웃음발언: {l}개")
+print(f"질문발언: {q}개")`
             },
             {
                 number: 10,
-                code: `s, n = input(), int(input())
-dic = {}
-for i in range(len(s) - n + 1):
-\tgram = s[i:i + n]
-\tdic[gram] = dic.get(gram, 0) + 1
-for key, value in sorted(dic.items()):
-\tprint(key, value)`
+                code: `n = int(input())
+arr, long = [], 0
+a, b, c, d = 0, 0, 0, 0
+
+for _ in range(n):
+    name, content = input().split('/')
+
+    if "강아지" in content or "고양이" in content or "동생" in content:
+        arr.append((name, "동물/가족 핑계", len(content)))
+        a += 1
+    elif "컴퓨터" in content or "인터넷" in content or "전기" in content:
+        arr.append((name, "기계 핑계", len(content)))
+        b += 1
+    elif "아팠" in content or "병원" in content or "감기" in content:
+        arr.append((name, "건강 핑계", len(content)))
+        c += 1
+    else:
+        arr.append((name, "창의적 핑계", len(content)))
+        d += 1
+
+    if len(content) >= 50:
+        long += 1
+
+arr.sort(key=lambda x: -x[2])
+for n, t, l in arr:
+    print(f"{n}: {t} (길이{l}자)")
+print(f"동물/가족:{a} 기계:{b} 건강:{c} 창의:{d} 장문:{long}")`
             },
             {
                 number: 11,
-                code: `s, n = input(), int(input())
-dic = {}
-for i in range(len(s) - n + 1):
-\tgram = s[i:i+n]
-\tdic[gram] = dic.get(gram, 0) + 1
-print(sorted(dic.items(), key=lambda x:(-x[1], x[0]))[0][0])`
-            },
-            {
-                number: 12,
-                code: `s, n = input().split(), int(input())
-grams = list(zip(*[s[i:] for i in range(n)]))
-print(*[' '.join(idx) for idx in grams], sep='\\n')`
-            },
-            {
-                number: 13,
-                code: `a, b, n = input(), input(), int(input())
-grams_a = set(zip(*[a[i:] for i in range(n)]))
-grams_b = set(zip(*[b[i:] for i in range(n)]))
-both = sorted(list(grams_a & grams_b))
-if both:
-\tfor i in both:
-\t\tprint(*i, sep='')
-else:
-\tprint("NONE")`
-            },
-            {
-                number: 14,
-                code: `def is_palindrome(word):
-\tfor i in range(len(word) // 2):
-\t\tif word[i] != word[-(i + 1)]:
-\t\t\treturn False
-\treturn True
-
-with open('data/words.txt', 'r') as f:
-\tarr = f.read().split('\\n')
-n, m = map(int, input().split())
-palin = [idx for idx in arr[n - 1:m] if is_palindrome(idx)]
-if palin:
-\tprint(*palin, sep='\\n')
-else:
-\tprint("NONE")`
-            },
-            {
-                number: 15,
-                code: `def solution(a, b):
-\treturn a + b`
-            },
-            {
-                number: 16,
-                code: `def solution(name):
-\tprint(f"안녕, {name}!")`
-            },
-            {
-                number: 17,
-                code: `def solution(lang):
-\tif lang == "Korean":
-\t\tprint("안녕하세요!")
-\telif lang == "English":
-\t\tprint("Hello!")
-\telif lang == "Japanese":
-\t\tprint("こんにちは!")
-\telse:
-\t\tprint("???")`
-            },
-            {
-                number: 18,
-                code: `def solution(n):
-\treturn (n // 2) * (n // 2 + 1)`
-            },
-            {
-                number: 19,
-                code: `def solution(a, b):
-\treturn a + b, a - b, a * b, a // b`
-            },
-            {
-                number: 20,
-                code: `def solution(s):
-\treturn s[::-1]`
-            },
-            {
-                number: 21,
-                code: `def solution(n):
-\tfor i in range(n):
-\t\tprint('*' * (i * 2 + 1))`
-            },
-            {
-                number: 22,
-                code: `def is_prime(num):
-\tn = num
-\ti = 2
-\twhile n > 1:
-\t\tif i == num:
-\t\t\treturn True
-\t\telif i == n:
-\t\t\treturn False
-\t\t
-\t\tif n / i == n // i:
-\t\t\tn //= i
-\t\t\ti = 2
-\t\telse:
-\t\t\ti += 1
-
-def solution(a, b):
-\tans = 0
-\tfor i in range(a, b + 1):
-\t\tif is_prime(i):
-\t\t\tans += 1
-\treturn ans`
-            },
-            {
-                number: 23,
-                code: `menu = {
-\t"떡볶이": 4000,
-\t"순대": 3000,
-\t"튀김": 2500,
-\t"김밥": 3500,
-\t"라면": 4500,
-}
-
-def get_total(*prices):
-\treturn sum(prices)
-
-def get_discount(total, rate=10):
-\treturn int(total * (rate / 100))
-
-def print_receipt(name, discount_rate, order):
-\tprice = get_total(*[menu[key] * value for key, value in order.items()])
-\tdiscounted = get_discount(price, discount_rate)
-\tprint(f"손님: {name}")
-\tprint("---")
-\tprint(f"합계: {price}원")
-\tprint(f"할인({discount_rate}%): -{discounted}원")
-\tprint(f"최종: {price - discounted}원")`
+                code: `def solution(**kwargs):
+    print("<캐릭터 카드>")
+    for i in ['이름', '직업', '레벨', '무기']:
+        print(f"{i}: {kwargs[i]}")`
             }
         ]
     }
