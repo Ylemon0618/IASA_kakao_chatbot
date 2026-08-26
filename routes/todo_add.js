f8822720b4fa9e2c0472d4585816b6bcb99bb63a
ylemon <kimkyuwan0618@gmail.com>
2026-08-26 15:54:35 +0900

    test: Test commit for todo_add.js

--- routes/todo_add.js
+++ routes/todo_add.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Todo_add = require("../models/Todo");
+const Todo = require("../models/Todo");
 const router = express.Router();
 const {saveLog, printError} = require('../utils/logger');
 
@@ -9,12 +9,19 @@ router.post('/', async (req, res) => {
     const userId = req.body.userRequest.user.id;
 
     try {
-        console.log(req.body.action.params.start_date);
-        console.log(req.body.action.params.end_date);
+        const start_date = req.body.action.params.start_date;
+        const end_date = req.body.action.params.end_date;
+
+        return res.json({
+            version: "2.0",
+            template: {
+                outputs: [{ simpleText: { text: `${start_date} ${end_date}` } },]
+            }
+        });
     } catch (error) {
         return res.json(printError(
-            './routes/goorm.js',
-            'Error while printing goorm answer',
+            './routes/todo_add.js',
+            'Error while adding todo',
             '오류가 발생했습니다.\n잠시 후에 다시 시도 해 주세요.'
         ));
     }
