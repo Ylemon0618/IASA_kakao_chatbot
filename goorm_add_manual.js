bbd75fe359b87864290ad89071c748c3d5740fa4
ylemon <kimkyuwan0618@gmail.com>
2026-09-10 16:41:56 +0900

    chore: edit goorm answer

--- goorm_add_manual.js
+++ goorm_add_manual.js
@@ -17,70 +17,345 @@ async function run() {
             problems: [
                 {
                     number: 1,
-                    code: `class Note:
-\tdef __init__(self):
-\t\tself.content = ''
+                    code: `class BankAccount:
+\tdef __init__(self, compound, simple, current):
+\t\tself.compound = compound
+\t\tself.simple = simple
+\t\tself.current = current
 
-\tdef write_content(self, content):
-\t\tself.content += content
+\tdef deposit(self, amount):
+\t\tself.current += amount
 
-\tdef remove_content(self):
-\t\tself.content = ''
-
-\tdef __str__(self):
-\t\treturn self.content
-
-class Notebook:
-\tdef __init__(self, title):
-\t\tself.title = title
-\t\tself.page_number = 0
-\t\tself.notes = {}
-
-\tdef add_note(self, note):
-\t\tif self.page_number == 300:
-\t\t\tprint("더 이상 노트를 추가할 수 없습니다.")
+\tdef withdraw(self, amount):
+\t\tif amount > self.current:
+\t\t\tprint("출금할 수 없습니다")
 \t\telse:
-\t\t\tself.page_number += 1
-\t\t\tself.notes[self.page_number] = note`
+\t\t\tself.current -= amount
+
+\tdef print_expected(self, years):
+\t\tcompound_new = BankAccount.calculator(self.compound, 0.03, years, True)
+\t\tsimple_new = BankAccount.calculator(self.simple, 0.05, years, False)
+
+\t\tprint(float(compound_new + simple_new + self.current))
+
+\t@staticmethod
+\tdef calculator(amount, rate, years, is_compound):
+\t\tif is_compound:
+\t\t\treturn amount * (1 + rate) ** years
+\t\telse:
+\t\t\treturn amount + years * rate * amount
+
+
+a, b, c, d, e, f = map(int, input().split())
+bank = BankAccount(a, b, c)
+bank.deposit(d)
+bank.withdraw(e)
+bank.print_expected(f)`
                 },
                 {
                     number: 2,
-                    code: `from abc import *
-class Post(ABC):
-\ttotal_posts = 0
+                    code: `class Island:
+    def __init__(self, idx):
+        self.idx = idx
+        self.bridges = []
+        self.treasure = 0
 
-\tdef __init__(self, author):
-\t\tself.author = author
-\t\tPost.total_posts += 1
+    def add_bridge(self, bridge):
+        self.bridges.append(bridge)
 
-\t@abstractmethod
-\tdef preview(self):
-\t\tpass
+    def move(self, visited):
+        for bridge in self.bridges:
+            dest, cost = bridge.get_dest(self)
+            if dest.idx not in visited:
+                return dest, cost
+        return None, None
 
-\t@classmethod
-\tdef count(cls):
-\t\treturn cls.total_posts
 
-\t@staticmethod
-\tdef is_valid_tag(tag):
-\t\treturn tag.startswith('#') and len(tag) >= 
+class Bridge:
+    def __init__(self, cost, *island):
+        self.island = island
+        self.cost = cost
 
-class TextPost(Post):
-\tdef __init__(self, author, text):
-\t\tsuper().__init__(author)
-\t\tself.text = text
+    def get_dest(self, curr_island):
+        dest = self.island[0] if self.island[0] != curr_island else self.island[1]
+        return dest, self.cost
 
-\tdef preview(self):
-\t\treturn f"[글] {self.author}: {self.text[:10]}..."
 
-class PhotoPost(Post):
-\tdef __init__(self, author, photo_count):
-\t\tsuper().__init__(author)
-\t\tself.photo_count = photo_count
+def simulate(n, k, islands):
+    p = [islands[1].treasure] if islands[1].treasure else []
+    q = [(islands[1], p, [1], k)]
+    res = []
 
-\tdef preview(self):
-\t\treturn f"[사진] {self.author}: 사진 {self.photo_count}장"`
-                }
+    while q:
+        cur, passed, visited, coin = q.pop()
+        passed, visited = passed[:], visited[:]
+
+        if cur.idx == n:
+            res.append(coin)
+            continue
+
+        nxt, cost = cur.move(visited)
+        if not nxt:
+            continue
+
+        if coin < cost:
+            if not passed:
+                continue
+
+            for i in range(len(passed)):
+                new_passed = passed[:]
+                new_coin = new_passed.pop(i)
+                q.append((cur, new_passed, visited, coin + new_coin))
+        else:
+            visited.append(nxt.idx)
+            if nxt.treasure:
+                passed.append(nxt.treasure)
+            q.append((nxt, passed, visited, coin - cost))
+
+    return min(res) if res else -1
+
+
+def get_input():
+    n = int(input())
+    m, k = map(int, input().split())
+
+    islands = []
+    for i in range(n + 1):
+        islands.append(Island(i))
+
+    for _ in range(n - 1):
+        a, b, c = map(int, input().split())
+        br = Bridge(c, islands[a], islands[b])
+        islands[a].add_bridge(br)
+        islands[b].add_bridge(br)
+
+    for _ in range(m):
+        d, g = map(int, input().split())
+        islands[d].treasure = g
+
+    return n, m, k, islands
+
+
+def main():
+    n, m, k, islands = get_input()
+
+    res = simulate(n, k, islands)
+
+    print(res)
+
+
+if __name__ == '__main__':
+    main()`
+                },
+                {
+                    number: 3,
+                    code: `class Medicine:
+\tdef __init__(self, name, max_count):
+\t\tself.count = 0
+\t\tself.name = name
+\t\tself.max_count = max_count
+
+\tdef take(self):
+\t\tself.count += 1
+\t\tprint(f"\\n{self.name}을(를) 복용했습니다.")
+
+\t\tif self.count == self.max_count:
+\t\t\tprint("오늘 복용 완료")
+\t\telse:
+\t\t\tprint(f"남은 복용 횟수: {self.max_count - self.count} 회")
+
+name, max_count = input(), int(input())
+medicine = Medicine(name, max_count)
+
+for _ in range(max_count):
+\tmedicine.take()`
+                },
+                {
+                    number: 4,
+                    code: `class Book:
+\tdef __init__(self, title, stock):
+\t\tself.title = title
+\t\tself.__stock = stock
+
+\tdef restock(self, amount):
+\t\tself.__stock += amount
+
+\tdef sell(self, amount):
+\t\tif self.__stock >= amount:
+\t\t\tself.__stock -= amount
+\t\t\treturn True
+\t\treturn False
+
+\tdef get_stock(self):
+\t\treturn self.__stock
+
+class EBook(Book):
+\tdef __init__(self, title):
+\t\tsuper().__init__(title, 999999)
+
+\tdef sell(self, amount):
+\t\treturn True
+
+title, stock = input().rsplit(' ', 1)
+book = Book(title, int(stock))
+
+n = int(input())
+for _ in range(n):
+    command, amount = input().split()
+    amount = int(amount)
+    if command == "RESTOCK":
+        book.restock(amount)
+    elif command == "SELL":
+        print("판매 성공" if book.sell(amount) else "판매 실패")
+
+print(f"최종 재고: {book.get_stock()}")`
+                },
+                {
+                    number: 5,
+                    code: `class Explorer:
+\tdef __init__(self, name, battery):
+\t\tself.name = name
+\t\tself.battery = battery
+
+\tdef status(self):
+\t\treturn "탐사 장비가 정상 작동합니다."
+
+class Submarine(Explorer):
+\tdef __init__(self, name, battery, depth):
+\t\tsuper().__init__(name, battery)
+\t\tself.depth = depth
+
+\tdef dive(self):
+\t\treturn f"{depth}m까지 잠수합니다."`
+                },
+                {
+                    number: 6,
+                    code: `class Person:
+\tdef __init__(self, name):
+\t\tself.name = name
+\t\tself.ac_hours = 0
+
+\tdef turn_on_ac(self, hours):
+\t\tself.ac_hours += hours
+
+class Household(Person):
+\trate_per_hour = 1000
+
+\tdef __init__(self, name):
+\t\tsuper().__init__(name)
+\t\tself.family = []
+
+\tdef add_family(self, person):
+\t\tself.family.append(person)
+
+\tdef calculate_bill(self):
+\t\thours = self.ac_hours
+\t\tfor person in self.family:
+\t\t\thours += person.ac_hours
+\t\treturn hours * Household.rate_per_hour`
+                },
+                {
+                    number: 7,
+                    code: `class Character:
+\tdef __init__(self, name):
+\t\tself.name = name
+\t\tself.hp = 100
+
+\tdef attacked(self, amount):
+\t\tself.hp = max(0, self.hp - amount)
+
+\tdef heal(self, amount):
+\t\tself.hp = min(100, self.hp + amount)
+
+c, n = map(int, input().split())
+characters = {name:Character(name) for name in input().split()}
+
+for _ in range(n):
+\tname, event, amount = input().split()
+\tif event == 'A':
+\t\tcharacters[name].attacked(int(amount))
+\telse:
+\t\tcharacters[name].heal(int(amount))
+
+dead = []
+for key, value in characters.items():
+\tif value.hp == 0:
+\t\tdead.append(key)
+if dead:
+\tprint(*sorted(dead), sep='\\n')
+else:
+\tprint('Alive')
+`
+                },
+                {
+                    number: 8,
+                    code: `class Item:
+\tdef __init__(self, parent, name):
+\t\tself.parent = parent
+\t\tself.name = name
+
+\tdef up(self):
+\t\treturn self.parent if self.parent else self
+
+class Folder(Item):
+\tdef __init__(self, parent, name):
+\t\tsuper().__init__(parent, name)
+\t\tself.folders = []
+\t\tself.files = []
+
+\tdef makefolder(self, name):
+\t\tself.folders.append(Folder(self, name))
+
+\tdef makefile(self, name, size):
+\t\tself.files.append(File(self, name, int(size)))
+
+\tdef down(self, name):
+\t\tfor i in self.folders:
+\t\t\tif i.name == name:
+\t\t\t\treturn i
+
+\tdef printsize(self):
+\t\tq, size = [self], 0
+\t\twhile q:
+\t\t\tnow = q.pop()
+\t\t\tsize += sum([i.size for i in now.files])
+\t\t\tq += now.folders
+\t\tprint(size)
+
+\tdef printcount(self):
+\t\tq, count = [self], 0
+\t\twhile q:
+\t\t\tnow = q.pop()
+\t\t\tcount += len(now.files)
+\t\t\tq += now.folders
+\t\tprint(count)
+
+class File(Item):
+\tdef __init__(self, parent, name, size):
+\t\tsuper().__init__(parent, name)
+\t\tself.size = size
+
+m = int(input())
+root = Folder(None, "root")
+now = root
+
+for _ in range(m):
+\tdata = input().split()
+\tcommand, args = data[0], data[1:]
+
+\tif command == "makefolder":
+\t\tnow.makefolder(*args)
+\telif command == "makefile":
+\t\tnow.makefile(*args)
+\telif command == "down":
+\t\tnow = now.down(*args)
+\telif command == "up":
+\t\tnow = now.up(*args)
+\telif command == "printsize":
+\t\tnow.printsize()
+\telif command == "printcount":
+\t\tnow.printcount()`
+                },
             ]
         });
 
