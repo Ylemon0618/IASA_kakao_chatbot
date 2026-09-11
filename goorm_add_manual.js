e69598922e2b489e33eefef27d308e0acf707009
ylemon <kimkyuwan0618@gmail.com>
2026-09-11 15:52:56 +0900

    chore: edit goorm answer

--- goorm_add_manual.js
+++ goorm_add_manual.js
@@ -1,367 +1,316 @@
-require('dotenv').config({ quiet: true });
+require('dotenv').config({quiet: true});
 const mongoose = require('mongoose');
 const Goorm = require("./models/Goorm");
 
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
 async function run() {
     try {
+        const problems = [
+            {
+                number: 9,
+                code: `class SchoolBag:
+\tdef __init__(self, items):
+\t\tself.__items = items
+
+\tdef check_item(self, item):
+\t\tif item in self.__items:
+\t\t\tself.__items.remove(item)
+\t\t\tprint(f"{item} 챙겼습니다!")
+\t\telse:
+\t\t\tprint("이미 챙겼거나 없는 물건입니다.")
+
+\tdef get_remaining(self):
+\t\treturn self.__items
+
+school_bag = SchoolBag(input().split())
+for _ in range(int(input())):
+\tschool_bag.check_item(input())
+print(school_bag.get_remaining())`
+            },
+            {
+                number: 10,
+                code: `class Star:
+\tdef __init__(self, name, magnitude, distance):
+\t\tself.name = name
+\t\tself.magnitude = magnitude
+\t\tself.distance = distance
+
+\tdef print_info(self):
+\t\tprint(f"이름: {self.name}, 겉보기 등급: {self.magnitude}등급, 거리: {self.distance}pc")
+
+n, m, d = input().split()
+star = Star(n, float(m), float(d))
+star.print_info()`
+            },
+            {
+                number: 11,
+                code: `class BasketballPlayer:
+\tdef __init__(self, name):
+\t\tself.name = name
+\t\tself.__points = 0
+
+\tdef score(self, points):
+\t\tself.__points += points
+
+\tdef get_points(self):
+\t\treturn self.__points
+
+name = input()
+player = BasketballPlayer(name)
+for _ in range(int(input())):
+\tplayer.score(int(input()))
+
+print(f"{player.name} 선수의 총 득점은 {player.get_points()}점입니다.")`
+            },
+            {
+                number: 12,
+                code: `class Hello:
+\tdef __init__(self, name):
+\t\tself.name = name
+
+\tdef greet(self):
+\t\tprint(f"{self.name}님, 안녕하세요!")`
+            },
+            {
+                number: 13,
+                code: `class Membership:
+\ttotal_members = 0
+\t
+\tdef __init__(self, name):
+\t\tself.name = name
+\t\tself.__point = 0
+\t\tself.scale = 1
+\t\t
+\t\tMembership.total_members += 1
+
+\tdef add_point(self, amount):
+\t\tself.__point += int(amount * self.scale)
+
+\tdef get_point(self):
+\t\treturn self.__point
+
+\t@classmethod
+\tdef get_total_members(cls):
+\t\treturn cls.total_members
+
+\tdef set_scale(self, scale):
+\t\tself.scale = scale
+
+class PremiumMember(Membership):
+\tdef __init__(self, name):
+\t\tsuper().__init__(name)
+\t\tsuper().set_scale(1.5)`
+            },
+            {
+                number: 14,
+                code: `class Team:
+\ttotal_team_goals = 0
+\ttotal_team_assists = 0
+
+\tdef __init__(self, name, goals, assists):
+\t\tself.name = name
+\t\tself.goals = goals
+\t\tself.assists = assists
+\t\t
+\t\tTeam.total_team_goals += goals
+\t\tTeam.total_team_assists += assists
+
+players = []
+for _ in range(int(input())):
+\tname, goals, assists = input().split()
+\tplayers.append(Team(name, int(goals), int(assists)))
+
+print(Team.total_team_goals)
+print(Team.total_team_assists)
+print(Team.total_team_goals + Team.total_team_assists)`
+            },
+            {
+                number: 15,
+                code: `class Gem:
+\tdef __init__(self, value, danger):
+\t\tself.value = value
+\t\tself.danger = danger
+
+class Thief(Gem):
+\tdef __init__(self, limit):
+\t\tsuper().__init__(0, 0)
+\t\tself.limit = limit
+
+\tdef steal(self, gem):
+\t\tif self.danger + gem.danger <= self.limit:
+\t\t\tself.value += gem.value
+\t\tself.danger += gem.danger
+
+l, n = map(int, input().split())
+thief = Thief(l)
+for _ in range(n):
+\tv, d = map(int, input().split())
+\tgem = Gem(v, d)
+\tthief.steal(gem)
+
+print(thief.value)`
+            },
+            {
+                number: 16,
+                code: `class Player:
+\tdef __init__(self, name, back_number):
+\t\tself.name = name
+\t\tself.back_number = back_number
+
+\tdef print_info(self):
+\t\tprint(f"{self.back_number}번 선수 {self.name}")
+
+player = Player(input(), int(input()))
+player.print_info()`
+            },
+            {
+                number: 17,
+                code: `class student:
+\tdef __init__(self, name, english_score, science_score, math_score):
+\t\tself.name = name
+\t\tself.english_score = english_score
+\t\tself.science_score = science_score
+\t\tself.math_score = math_score
+
+\tdef get_average(self):
+\t\treturn (self.english_score + self.science_score + self.math_score) / 3
+
+\tdef get_grade(self):
+\t\taverage = self.get_average()
+\t\tif average >= 90:
+\t\t\treturn 'A'
+\t\telif average >= 80:
+\t\t\treturn 'C'
+\t\telif average >= 70:
+\t\t\treturn 'D'
+\t\telif average >= 60:
+\t\t\treturn 'X'
+\t\telse:
+\t\t\treturn '희중이 바보'
+
+data = input().split()
+name, scores = data[0], list(map(int, data[1:]))
+
+s = student(name, *scores)
+print(f"{s.name} {s.get_average()} {s.get_grade()}")`
+            },
+            {
+                number: 18,
+                code: `class BankAccount:
+\tdef __init__(self, name, balance):
+\t\tself.name = name
+\t\tself.balance = balance
+
+\tdef deposit(self, amount):
+\t\tself.balance += amount
+
+\tdef withdraw(self, amount):
+\t\tself.balance -= amount
+
+\tdef check(self):
+\t\tprint(self.balance)
+
+n, b = input().split()
+bank = BankAccount(n, int(b))
+
+for _ in range(int(input())):
+\tdata = input().split()
+\tif data[0] == 'deposit':
+\t\tbank.deposit(int(data[1]))
+\telif data[0] == 'withdraw':
+\t\tbank.withdraw(int(data[1]))
+\telse:
+\t\tbank.check()`
+            },
+            {
+                number: 19,
+                code: `class Character:
+\ttotal_characters = 0
+\t
+\tdef __init__(self, name, hp, gold):
+\t\tself.name = name
+\t\tself.hp = hp
+\t\tself.__gold = gold
+\t\tCharacter.total_characters += 1
+
+\tdef get_gold(self):
+\t\treturn self.__gold
+
+\tdef add_gold(self, amount):
+\t\tself.__gold += amount
+
+\t@staticmethod
+\tdef is_alive(hp):
+\t\treturn hp > 0
+
+\t@classmethod
+\tdef get_total_count(cls):
+\t\treturn cls.total_characters
+
+class Wizard(Character):
+\tdef __init__(self, name, hp, mp, gold):
+\t\tsuper().__init__(name, hp, gold)
+\t\tself.mp = mp
+
+\tdef cast_spell(self):
+\t\tprint(f"{self.name}이(가) 마법을 시전합니다! (남은 MP: {self.mp})")
+
+data = input().split()
+c = Wizard(data[0], *list(map(int, data[1:])))
+
+c.cast_spell()
+print(f"생존 여부: {c.is_alive(c.hp)}")
+c.add_gold(500)
+print(f"현재 소지금: {c.get_gold()} G")
+print(f"생성된 총 캐릭터 수: {c.get_total_count()}명")`
+            },
+        ]
+
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected');
 
-        const latestGoorm = await Goorm.findOne({ week: { $exists: true } }).sort({ week: -1 });
-        const nextWeek = latestGoorm?.week ? latestGoorm.week + 1 : 1;
-
-        console.log(`생성될 주차(week): ${nextWeek}`);
-
-        const inserted = await Goorm.create({
-            week: nextWeek,
-            problems: [
-                {
-                    number: 1,
-                    code: `class BankAccount:
-\tdef __init__(self, compound, simple, current):
-\t\tself.compound = compound
-\t\tself.simple = simple
-\t\tself.current = current
-
-\tdef deposit(self, amount):
-\t\tself.current += amount
-
-\tdef withdraw(self, amount):
-\t\tif amount > self.current:
-\t\t\tprint("출금할 수 없습니다")
-\t\telse:
-\t\t\tself.current -= amount
-
-\tdef print_expected(self, years):
-\t\tcompound_new = BankAccount.calculator(self.compound, 0.03, years, True)
-\t\tsimple_new = BankAccount.calculator(self.simple, 0.05, years, False)
-
-\t\tprint(float(compound_new + simple_new + self.current))
-
-\t@staticmethod
-\tdef calculator(amount, rate, years, is_compound):
-\t\tif is_compound:
-\t\t\treturn amount * (1 + rate) ** years
-\t\telse:
-\t\t\treturn amount + years * rate * amount
-
-
-a, b, c, d, e, f = map(int, input().split())
-bank = BankAccount(a, b, c)
-bank.deposit(d)
-bank.withdraw(e)
-bank.print_expected(f)`
-                },
-                {
-                    number: 2,
-                    code: `class Island:
-    def __init__(self, idx):
-        self.idx = idx
-        self.bridges = []
-        self.treasure = 0
-
-    def add_bridge(self, bridge):
-        self.bridges.append(bridge)
-
-    def move(self, visited):
-        for bridge in self.bridges:
-            dest, cost = bridge.get_dest(self)
-            if dest.idx not in visited:
-                return dest, cost
-        return None, None
-
-
-class Bridge:
-    def __init__(self, cost, *island):
-        self.island = island
-        self.cost = cost
-
-    def get_dest(self, curr_island):
-        dest = self.island[0] if self.island[0] != curr_island else self.island[1]
-        return dest, self.cost
-
-
-def simulate(n, k, islands):
-    p = [islands[1].treasure] if islands[1].treasure else []
-    q = [(islands[1], p, [1], k)]
-    res = []
-
-    while q:
-        cur, passed, visited, coin = q.pop()
-        passed, visited = passed[:], visited[:]
-
-        if cur.idx == n:
-            res.append(coin)
-            continue
-
-        nxt, cost = cur.move(visited)
-        if not nxt:
-            continue
-
-        if coin < cost:
-            if not passed:
-                continue
-
-            for i in range(len(passed)):
-                new_passed = passed[:]
-                new_coin = new_passed.pop(i)
-                q.append((cur, new_passed, visited, coin + new_coin))
-        else:
-            visited.append(nxt.idx)
-            if nxt.treasure:
-                passed.append(nxt.treasure)
-            q.append((nxt, passed, visited, coin - cost))
-
-    return min(res) if res else -1
-
-
-def get_input():
-    n = int(input())
-    m, k = map(int, input().split())
-
-    islands = []
-    for i in range(n + 1):
-        islands.append(Island(i))
-
-    for _ in range(n - 1):
-        a, b, c = map(int, input().split())
-        br = Bridge(c, islands[a], islands[b])
-        islands[a].add_bridge(br)
-        islands[b].add_bridge(br)
-
-    for _ in range(m):
-        d, g = map(int, input().split())
-        islands[d].treasure = g
-
-    return n, m, k, islands
-
-
-def main():
-    n, m, k, islands = get_input()
-
-    res = simulate(n, k, islands)
-
-    print(res)
-
-
-if __name__ == '__main__':
-    main()`
-                },
-                {
-                    number: 3,
-                    code: `class Medicine:
-\tdef __init__(self, name, max_count):
-\t\tself.count = 0
-\t\tself.name = name
-\t\tself.max_count = max_count
-
-\tdef take(self):
-\t\tself.count += 1
-\t\tprint(f"\\n{self.name}을(를) 복용했습니다.")
-
-\t\tif self.count == self.max_count:
-\t\t\tprint("오늘 복용 완료")
-\t\telse:
-\t\t\tprint(f"남은 복용 횟수: {self.max_count - self.count} 회")
-
-name, max_count = input(), int(input())
-medicine = Medicine(name, max_count)
-
-for _ in range(max_count):
-\tmedicine.take()`
-                },
-                {
-                    number: 4,
-                    code: `class Book:
-\tdef __init__(self, title, stock):
-\t\tself.title = title
-\t\tself.__stock = stock
-
-\tdef restock(self, amount):
-\t\tself.__stock += amount
-
-\tdef sell(self, amount):
-\t\tif self.__stock >= amount:
-\t\t\tself.__stock -= amount
-\t\t\treturn True
-\t\treturn False
-
-\tdef get_stock(self):
-\t\treturn self.__stock
-
-class EBook(Book):
-\tdef __init__(self, title):
-\t\tsuper().__init__(title, 999999)
-
-\tdef sell(self, amount):
-\t\treturn True
-
-title, stock = input().rsplit(' ', 1)
-book = Book(title, int(stock))
-
-n = int(input())
-for _ in range(n):
-    command, amount = input().split()
-    amount = int(amount)
-    if command == "RESTOCK":
-        book.restock(amount)
-    elif command == "SELL":
-        print("판매 성공" if book.sell(amount) else "판매 실패")
-
-print(f"최종 재고: {book.get_stock()}")`
-                },
-                {
-                    number: 5,
-                    code: `class Explorer:
-\tdef __init__(self, name, battery):
-\t\tself.name = name
-\t\tself.battery = battery
-
-\tdef status(self):
-\t\treturn "탐사 장비가 정상 작동합니다."
-
-class Submarine(Explorer):
-\tdef __init__(self, name, battery, depth):
-\t\tsuper().__init__(name, battery)
-\t\tself.depth = depth
-
-\tdef dive(self):
-\t\treturn f"{depth}m까지 잠수합니다."`
-                },
-                {
-                    number: 6,
-                    code: `class Person:
-\tdef __init__(self, name):
-\t\tself.name = name
-\t\tself.ac_hours = 0
-
-\tdef turn_on_ac(self, hours):
-\t\tself.ac_hours += hours
-
-class Household(Person):
-\trate_per_hour = 1000
-
-\tdef __init__(self, name):
-\t\tsuper().__init__(name)
-\t\tself.family = []
-
-\tdef add_family(self, person):
-\t\tself.family.append(person)
-
-\tdef calculate_bill(self):
-\t\thours = self.ac_hours
-\t\tfor person in self.family:
-\t\t\thours += person.ac_hours
-\t\treturn hours * Household.rate_per_hour`
-                },
-                {
-                    number: 7,
-                    code: `class Character:
-\tdef __init__(self, name):
-\t\tself.name = name
-\t\tself.hp = 100
-
-\tdef attacked(self, amount):
-\t\tself.hp = max(0, self.hp - amount)
-
-\tdef heal(self, amount):
-\t\tself.hp = min(100, self.hp + amount)
-
-c, n = map(int, input().split())
-characters = {name:Character(name) for name in input().split()}
-
-for _ in range(n):
-\tname, event, amount = input().split()
-\tif event == 'A':
-\t\tcharacters[name].attacked(int(amount))
-\telse:
-\t\tcharacters[name].heal(int(amount))
-
-dead = []
-for key, value in characters.items():
-\tif value.hp == 0:
-\t\tdead.append(key)
-if dead:
-\tprint(*sorted(dead), sep='\\n')
-else:
-\tprint('Alive')
-`
-                },
-                {
-                    number: 8,
-                    code: `class Item:
-\tdef __init__(self, parent, name):
-\t\tself.parent = parent
-\t\tself.name = name
-
-\tdef up(self):
-\t\treturn self.parent if self.parent else self
-
-class Folder(Item):
-\tdef __init__(self, parent, name):
-\t\tsuper().__init__(parent, name)
-\t\tself.folders = []
-\t\tself.files = []
-
-\tdef makefolder(self, name):
-\t\tself.folders.append(Folder(self, name))
-
-\tdef makefile(self, name, size):
-\t\tself.files.append(File(self, name, int(size)))
-
-\tdef down(self, name):
-\t\tfor i in self.folders:
-\t\t\tif i.name == name:
-\t\t\t\treturn i
-
-\tdef printsize(self):
-\t\tq, size = [self], 0
-\t\twhile q:
-\t\t\tnow = q.pop()
-\t\t\tsize += sum([i.size for i in now.files])
-\t\t\tq += now.folders
-\t\tprint(size)
-
-\tdef printcount(self):
-\t\tq, count = [self], 0
-\t\twhile q:
-\t\t\tnow = q.pop()
-\t\t\tcount += len(now.files)
-\t\t\tq += now.folders
-\t\tprint(count)
-
-class File(Item):
-\tdef __init__(self, parent, name, size):
-\t\tsuper().__init__(parent, name)
-\t\tself.size = size
-
-m = int(input())
-root = Folder(None, "root")
-now = root
-
-for _ in range(m):
-\tdata = input().split()
-\tcommand, args = data[0], data[1:]
-
-\tif command == "makefolder":
-\t\tnow.makefolder(*args)
-\telif command == "makefile":
-\t\tnow.makefile(*args)
-\telif command == "down":
-\t\tnow = now.down(*args)
-\telif command == "up":
-\t\tnow = now.up(*args)
-\telif command == "printsize":
-\t\tnow.printsize()
-\telif command == "printcount":
-\t\tnow.printcount()`
-                },
-            ]
+        let command;
+        rl.question('Insert new week(I) / Append existing week(A)', (answer) => {
+            command = answer;
+            rl.close();
         });
 
-        if (inserted) {
-            console.log("Answer successfully inserted");
+        if (command === 'I') {
+            const latestGoorm = await Goorm.findOne({week: {$exists: true}}).sort({week: -1});
+            const nextWeek = latestGoorm?.week ? latestGoorm.week + 1 : 1;
+
+            console.log(`Inserted week: ${nextWeek}`);
+
+            const inserted = await Goorm.create({
+                week: nextWeek,
+                problems: problems
+            });
+
+            if (inserted) {
+                console.log("Answer successfully inserted");
+            }
+        } else if (command === 'A') {
+            rl.question('Enter week number: ', async (answer) => {
+                const result = await Goorm.updateOne(
+                    {week: answer},
+                    {$push: {problems: problems}}
+                );
+
+                if (result.matchedCount === 0) {
+                    console.log(`Unable to find week ${answer}`);
+                    return false;
+                }
+
+                console.log(`Answer successfully appended`);
+                return true;
+            });
         }
+
     } catch (err) {
         console.error("Error occurred:", err.message);
     } finally {
