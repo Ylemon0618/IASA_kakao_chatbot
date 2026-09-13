f31f944517ce4e6a82edf98b79666dc4abec4bc2
ylemon <kimkyuwan0618@gmail.com>
2026-09-13 20:06:19 +0900

    chore: upload goorm answer

--- goorm_add_manual.js
+++ goorm_add_manual.js
@@ -18,262 +18,358 @@ async function run() {
     try {
         const problems = [
             {
-                number: 9,
-                code: `class SchoolBag:
-\tdef __init__(self, items):
-\t\tself.__items = items
+                number: 20,
+                code: `class DeliveryRider:
+\tdef __init__(self, rider_name):
+\t\tself.rider_name = rider_name
+\t\tself.rating = 4.5
 
-\tdef check_item(self, item):
-\t\tif item in self.__items:
-\t\t\tself.__items.remove(item)
-\t\t\tprint(f"{item} 챙겼습니다!")
-\t\telse:
-\t\t\tprint("이미 챙겼거나 없는 물건입니다.")
+\tdef accept_order(self, order_name):
+\t\treturn f"{self.rider_name}님이 {order_name} 주문을 수락했습니다."
 
-\tdef get_remaining(self):
-\t\treturn self.__items
+class SuspendedRider(DeliveryRider):
+\tdef __init__(self, rider_name):
+\t\tsuper().__init__(rider_name)
+\t\tself.rating = 2.0
 
-school_bag = SchoolBag(input().split())
-for _ in range(int(input())):
-\tschool_bag.check_item(input())
-print(school_bag.get_remaining())`
+\tdef accept_order(self, order_name):
+\t\treturn f"정지: {self.rider_name}님은 평점이 낮아 주문을 수락할 수 없습니다."
+
+class PremiumRider(DeliveryRider):
+\tdef __init__(self, rider_name, vehicle_type):
+\t\tsuper().__init__(rider_name)
+\t\tself.rating = 4.9
+\t\tself.vehicle_type = vehicle_type
+
+\tdef accept_order(self, order_name):
+\t\treturn f"[{self.vehicle_type} 프리미엄 라이더] {self.rider_name}님이 {order_name} 주문을 수락했습니다."`
             },
             {
-                number: 10,
-                code: `class Star:
-\tdef __init__(self, name, magnitude, distance):
+                number: 21,
+                code: `class Poke:
+\tdef __init__(self, name, Type):
 \t\tself.name = name
-\t\tself.magnitude = magnitude
-\t\tself.distance = distance
+\t\tself.Type = Type
+
+\tdef print_data(self):
+\t\tprint(f"이름: {self.name}, 타입: {self.Type}")
+
+pokemon = Poke(input(), input())
+pokemon.print_data()`
+            },
+            {
+                number: 22,
+                code: `class GradeAnalyzer:
+\tdef __init__(self, grades):
+\t\tself.grades = grades
+
+\tdef get_period(self, grade):
+\t\ttemp = []
+\t\tfor i in range(len(self.grades)):
+\t\t\tif self.grades[i] == grade:
+\t\t\t\ttemp.append(i)
+\t\tif len(temp) <= 1:
+\t\t\treturn 0
+\t\treturn temp[-1] - temp[0]
+
+n = int(input())
+grade = GradeAnalyzer(input())
+periods = [(i, grade.get_period(i)) for i in ['A', 'B', 'C', 'D', 'F']]
+periods.sort(key=lambda x: (-x[1], x[0]))
+print(*periods[0])`
+            },
+            {
+                number: 23,
+                code: `class DateCourse:
+\tdef __init__(self, name1, name2, love):
+\t\tself.name1 = name1
+\t\tself.name2 = name2
+\t\tself.love = love
+
+\tdef watch_movie(self, activity, point):
+\t\tself.love += point
+\t\tprint(f"{activity} 영화를 함께 보았습니다!")
+
+\tdef eat_dinner(self, activity, point):
+\t\tself.love += point
+\t\tprint(f"{self.name1}와(과) {self.name2}이(가) {activity}을(를) 맛있게 먹었습니다.")
+
+\tdef check_love(self):
+\t\tprint(f"현재 호감도: {self.love}점")`
+            },
+            {
+                number: 24,
+                code: `class epl:
+\tdef __init__(self, rank):
+\t\tself.rank = rank
+\t\tself.result_win = "not yet"
+\t\tself.result_ticket = ''
+
+\tdef result(self):
+\t\tif self.rank == 1:
+\t\t\tself.result_win = "premier league champions"
+
+\t\tif 1 <= self.rank <= 4:
+\t\t\tself.result_ticket = "champions league"
+\t\telif self.rank == 5:
+\t\t\tself.result_ticket = "europa league"
+\t\telif 6 <= self.rank <= 17:
+\t\t\tself.result_ticket = "premier league"
+\t\telse:
+\t\t\tself.result_ticket = "relegration"
 
 \tdef print_info(self):
-\t\tprint(f"이름: {self.name}, 겉보기 등급: {self.magnitude}등급, 거리: {self.distance}pc")
-
-n, m, d = input().split()
-star = Star(n, float(m), float(d))
-star.print_info()`
+\t\tprint(self.result_win)
+\t\tprint(self.result_ticket)`
             },
             {
-                number: 11,
-                code: `class BasketballPlayer:
-\tdef __init__(self, name):
-\t\tself.name = name
-\t\tself.__points = 0
-
-\tdef score(self, points):
-\t\tself.__points += points
-
-\tdef get_points(self):
-\t\treturn self.__points
-
-name = input()
-player = BasketballPlayer(name)
-for _ in range(int(input())):
-\tplayer.score(int(input()))
-
-print(f"{player.name} 선수의 총 득점은 {player.get_points()}점입니다.")`
-            },
-            {
-                number: 12,
-                code: `class Hello:
-\tdef __init__(self, name):
-\t\tself.name = name
-
-\tdef greet(self):
-\t\tprint(f"{self.name}님, 안녕하세요!")`
-            },
-            {
-                number: 13,
-                code: `class Membership:
-\ttotal_members = 0
-\t
-\tdef __init__(self, name):
-\t\tself.name = name
-\t\tself.__point = 0
-\t\tself.scale = 1
-\t\t
-\t\tMembership.total_members += 1
-
-\tdef add_point(self, amount):
-\t\tself.__point += int(amount * self.scale)
-
-\tdef get_point(self):
-\t\treturn self.__point
-
-\t@classmethod
-\tdef get_total_members(cls):
-\t\treturn cls.total_members
-
-\tdef set_scale(self, scale):
-\t\tself.scale = scale
-
-class PremiumMember(Membership):
-\tdef __init__(self, name):
-\t\tsuper().__init__(name)
-\t\tsuper().set_scale(1.5)`
-            },
-            {
-                number: 14,
-                code: `class Team:
-\ttotal_team_goals = 0
-\ttotal_team_assists = 0
-
-\tdef __init__(self, name, goals, assists):
-\t\tself.name = name
-\t\tself.goals = goals
-\t\tself.assists = assists
-\t\t
-\t\tTeam.total_team_goals += goals
-\t\tTeam.total_team_assists += assists
-
-players = []
-for _ in range(int(input())):
-\tname, goals, assists = input().split()
-\tplayers.append(Team(name, int(goals), int(assists)))
-
-print(Team.total_team_goals)
-print(Team.total_team_assists)
-print(Team.total_team_goals + Team.total_team_assists)`
-            },
-            {
-                number: 15,
-                code: `class Gem:
-\tdef __init__(self, value, danger):
-\t\tself.value = value
-\t\tself.danger = danger
-
-class Thief(Gem):
-\tdef __init__(self, limit):
-\t\tsuper().__init__(0, 0)
-\t\tself.limit = limit
-
-\tdef steal(self, gem):
-\t\tif self.danger + gem.danger <= self.limit:
-\t\t\tself.value += gem.value
-\t\tself.danger += gem.danger
-
-l, n = map(int, input().split())
-thief = Thief(l)
-for _ in range(n):
-\tv, d = map(int, input().split())
-\tgem = Gem(v, d)
-\tthief.steal(gem)
-
-print(thief.value)`
-            },
-            {
-                number: 16,
+                number: 25,
                 code: `class Player:
 \tdef __init__(self, name, back_number):
 \t\tself.name = name
 \t\tself.back_number = back_number
 
 \tdef print_info(self):
-\t\tprint(f"{self.back_number}번 선수 {self.name}")
+\t\tprint(f"이름: {self.name}, 등번호: {self.back_number}")
 
-player = Player(input(), int(input()))
-player.print_info()`
+class Pitcher(Player):
+\tdef __init__(self, name, back_number, earned_runs, innings):
+\t\tself.name = name
+\t\tself.back_number = back_number
+\t\tself.earned_runs = earned_runs
+\t\tself.innings = innings
+
+\tdef print_info(self):
+\t\tprint(f"이름: {self.name}, 등번호: {self.back_number}, 평균자책점: {self.earned_runs * 9 / self.innings:.2f}")`
             },
             {
-                number: 17,
-                code: `class student:
-\tdef __init__(self, name, english_score, science_score, math_score):
+                number: 26,
+                code: `class Station:
+\tdef __init__(self, name, d1, d2, diesel, gasoline):
 \t\tself.name = name
-\t\tself.english_score = english_score
-\t\tself.science_score = science_score
-\t\tself.math_score = math_score
+\t\tself.d1 = d1
+\t\tself.d2 = d2
+\t\tself.diesel = diesel
+\t\tself.gasoline = gasoline
 
-\tdef get_average(self):
-\t\treturn (self.english_score + self.science_score + self.math_score) / 3
+\tdef max_oiling(self, budget):
+\t\treturn (budget / self.diesel, budget / self.gasoline)
 
-\tdef get_grade(self):
-\t\taverage = self.get_average()
-\t\tif average >= 90:
-\t\t\treturn 'A'
-\t\telif average >= 80:
-\t\t\treturn 'C'
-\t\telif average >= 70:
-\t\t\treturn 'D'
-\t\telif average >= 60:
-\t\t\treturn 'X'
-\t\telse:
-\t\t\treturn '희중이 바보'
-
-data = input().split()
-name, scores = data[0], list(map(int, data[1:]))
-
-s = student(name, *scores)
-print(f"{s.name} {s.get_average()} {s.get_grade()}")`
-            },
-            {
-                number: 18,
-                code: `class BankAccount:
-\tdef __init__(self, name, balance):
+class Car:
+\tdef __init__(self, name, fuel, efficiency):
 \t\tself.name = name
-\t\tself.balance = balance
+\t\tself.fuel = fuel
+\t\tself.efficiency = efficiency
 
-\tdef deposit(self, amount):
-\t\tself.balance += amount
+\tdef max_distance(self):
+\t\treturn self.fuel * self.efficiency
 
-\tdef withdraw(self, amount):
-\t\tself.balance -= amount
+\tdef can_arrive_station(self, station):
+\t\tif station.d1 <= self.max_distance():
+\t\t\treturn self.fuel - station.d1 / self.efficiency
+\t\treturn False
 
-\tdef check(self):
-\t\tprint(self.balance)
+class DieselCar(Car):
+\tdef __init__(self, name, fuel, efficiency):
+\t\tsuper().__init__(name, fuel, efficiency)
 
-n, b = input().split()
-bank = BankAccount(n, int(b))
+\tdef can_arrive_destination(self, station, budget):
+\t\toil_left = self.can_arrive_station(station)
+\t\tif oil_left is False:
+\t\t\treturn False
+
+\t\toil_inneed = station.d2 / self.efficiency
+\t\toil_refuel = oil_inneed - oil_left
+\t\tif oil_refuel <= 0:
+\t\t\treturn 0
+\t\tif oil_refuel > station.max_oiling(budget)[0]:
+\t\t\treturn False
+\t\treturn oil_refuel * station.diesel
+
+class GasolineCar(Car):
+\tdef __init__(self, name, fuel, efficiency):
+\t\tsuper().__init__(name, fuel, efficiency)
+
+\tdef can_arrive_destination(self, station, budget):
+\t\toil_left = self.can_arrive_station(station)
+\t\tif oil_left is False:
+\t\t\treturn False
+
+\t\toil_inneed = station.d2 / self.efficiency
+\t\toil_refuel = oil_inneed - oil_left
+\t\tif oil_refuel <= 0:
+\t\t\treturn 0
+\t\tif oil_refuel > station.max_oiling(budget)[1]:
+\t\t\treturn False
+\t\treturn oil_refuel * station.gasoline
+
+def best_route(car, stations, budget):
+\tans, able_station = -1, None
+\tfor station in stations:
+\t\toil_cost = car.can_arrive_destination(station, budget)
+\t\tif oil_cost is not False and ans < budget - oil_cost:
+\t\t\tans = budget - oil_cost
+\t\t\table_station = station
+
+\tif able_station:
+\t\treturn f"{able_station.name} {ans:.2f}원 남음"
+\treturn "따릉이 타기"
+
+# 입출력처리 코드
+budget = int(input())
+N, M = map(int, input().split())
+
+cars, order = {}, []
+for _ in range(N):
+\tname, fuel_type, fuel, eff = input().split()
+\tcls = DieselCar if fuel_type == "경유" else GasolineCar
+\tcars[name] = cls(name, int(fuel), int(eff))
+\torder.append(name)
+
+stations, by_name = [], {}
+for _ in range(M):
+\tname, d1, d2, diesel, gasoline = input().split()
+\tst = Station(name, int(d1), int(d2), int(diesel), int(gasoline))
+\tstations.append(st)
+\tby_name[name] = st
 
 for _ in range(int(input())):
 \tdata = input().split()
-\tif data[0] == 'deposit':
-\t\tbank.deposit(int(data[1]))
-\telif data[0] == 'withdraw':
-\t\tbank.withdraw(int(data[1]))
+\tquestion = ' '.join(data[1:])
+
+\tif question == "최대 이동거리":
+\t\tcar = cars[data[0]]
+\t\tprint(f"{car.name} 최대 이동거리: {car.max_distance():.2f}km")
+\telif question == "최대 주유량":
+\t\tstation = by_name[data[0]]
+\t\tdiesel, gasoline = station.max_oiling(budget)
+\t\tprint(f"{station.name} 최대 주유량: 경유 {diesel:.2f}L / 휘발유 {gasoline:.2f}L")
+\telif question.endswith("주유소까지"):
+\t\tcar = cars[data[0]]
+\t\tstation = by_name[data[1]]
+\t\toil_left = car.can_arrive_station(station)
+\t\tif oil_left is False:
+\t\t\tprint(f"{car.name} -> {station.name}: 도달 불가")
+\t\telse:
+\t\t\tprint(f"{car.name} -> {station.name}: 남은 기름 {oil_left:.2f}L")
 \telse:
-\t\tbank.check()`
+\t\tcar = cars[data[0]]
+\t\tstation = by_name[data[1]]
+\t\tcost = car.can_arrive_destination(station, budget)
+\t\tif cost is False:
+\t\t\tprint(f"{car.name} -> {station.name} -> 도착지: 불가")
+\t\telse:
+\t\t\tprint(f"{car.name} -> {station.name} -> 도착지: {cost:.2f}원")
+
+print(f"[예산 {budget}원] 주유 계획")
+for name in order:
+\tcar = cars[name]
+\tprint(f"{car.name}: {best_route(car, stations, budget)}")`
             },
             {
-                number: 19,
+                number: 27,
+                code: `class Book:
+\tdef __init__(self, title, author):
+\t\tself.title = title
+\t\tself.author = author
+\t\tself.is_borrowed = False
+
+\tdef borrow(self):
+\t\tif self.is_borrowed:
+\t\t\treturn "이미 대출 중입니다."
+\t\tself.is_borrowed = True
+\t\treturn "대출되었습니다."
+
+\tdef return_book(self):
+\t\tif not self.is_borrowed:
+\t\t\treturn "대출되지 않은 책입니다."
+\t\tself.is_borrowed = False
+\t\treturn "반납되었습니다."
+
+\tdef get_info(self):
+\t\treturn f"{self.title} - {self.author} ({'대출중' if self.is_borrowed else '대출가능'})"`
+            },
+            {
+                number: 28,
+                code: `class VIPMember(Member):
+\tdef __init__(self, name, current_books, vip_days):
+\t\tsuper().__init__(name, current_books)
+\t\tself.vip_days = vip_days
+
+\tdef vip_borrow(self, amount):
+\t\tlimit = 5 + self.vip_days * 10
+\t\tif self.current_books + amount <= limit:
+\t\t\tself.current_books += amount
+\t\t\treturn f"VIP 대출 성공. 총 {self.current_books}권 대출 중입니다."
+\t\telse:
+\t\t\treturn "한도 초과로 대출할 수 없습니다."`
+            },
+            {
+                number: 29,
                 code: `class Character:
-\ttotal_characters = 0
+\tmax_level = 99
 \t
-\tdef __init__(self, name, hp, gold):
+\tdef __init__(self, name, level):
 \t\tself.name = name
-\t\tself.hp = hp
-\t\tself.__gold = gold
-\t\tCharacter.total_characters += 1
-
-\tdef get_gold(self):
-\t\treturn self.__gold
-
-\tdef add_gold(self, amount):
-\t\tself.__gold += amount
-
-\t@staticmethod
-\tdef is_alive(hp):
-\t\treturn hp > 0
+\t\tself.level = level
 
 \t@classmethod
-\tdef get_total_count(cls):
-\t\treturn cls.total_characters
+\tdef change_max_level(cls, new_max):
+\t\tcls.max_level = new_max
 
-class Wizard(Character):
-\tdef __init__(self, name, hp, mp, gold):
-\t\tsuper().__init__(name, hp, gold)
-\t\tself.mp = mp
+\t@staticmethod
+\tdef is_valid_name(name):
+\t\treturn len(name) >= 2
 
-\tdef cast_spell(self):
-\t\tprint(f"{self.name}이(가) 마법을 시전합니다! (남은 MP: {self.mp})")
+\tdef attack(self):
+\t\treturn f"{self.name}이(가) 일반 공격을 합니다!"
 
-data = input().split()
-c = Wizard(data[0], *list(map(int, data[1:])))
+class Mage(Character):
+\tdef __init__(self, name, level, mana):
+\t\tsuper().__init__(name, level)
+\t\tself.mana = mana
 
-c.cast_spell()
-print(f"생존 여부: {c.is_alive(c.hp)}")
-c.add_gold(500)
-print(f"현재 소지금: {c.get_gold()} G")
-print(f"생성된 총 캐릭터 수: {c.get_total_count()}명")`
+\tdef attack(self):
+\t\treturn f"{self.name}이(가) 마법 공격을 합니다! (소모 마나: {self.mana})"`
+            },
+            {
+                number: 30,
+                code: `class bnd_member:
+\tdef __init__(self, name, nicknames):
+\t\tself.name = name
+\t\tself.nicknames = nicknames
+
+\tdef has_nickname(self, nickname):
+\t\treturn nickname in self.nicknames
+
+class bnd_directory:
+\tdef __init__(self):
+\t\tself.members = []
+
+\tdef add_member(self, member):
+\t\tself.members.append(member)
+
+BOYNEXTDOOR = bnd_directory()
+member_data = {
+\t"운학": ["우나기", "눈사람"],
+\t"명재현": ["명뇨리따", "명명이"],
+\t"리우": ["대상혁", "애햄이", "두부 모서리"],
+\t"이한": ["제로제로이하나", "물고기 아빠"],
+\t"성호": ["예삐", "막기대라고 넓혀놓은 어깨 주인"],
+\t"태산": ["간ZI폭풍 GIANT MOUNTAIN", "부힛부힛사르르rrr탯냥이"]
+}
+for key, value in member_data.items():
+\tnew_member = bnd_member(key, value)
+\tBOYNEXTDOOR.add_member(new_member)
+
+nick = input()
+for member in BOYNEXTDOOR.members:
+\tif member.has_nickname(nick):
+\t\tprint(f"{nick} -> {member.name}의 별명입니다.")
+\t\tbreak
+else:
+\tprint("등록되지 않은 별명입니다.")
+`
             },
         ]
 
